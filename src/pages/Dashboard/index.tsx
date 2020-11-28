d8fa8fe5093e4f17177329ff60bffdddeab8f2c2
carlos <carloskauanmoreiradesousa@gmail.com>
2020-11-28 13:31:58 -0300

    update dashboard style and its structure

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/github-logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 export default function Dashboard(): JSX.Element {
-	return <Title>Explore repositórios no Github.</Title>;
+	return (
+		<>
+			<img src={logoImg} alt="Github Explorer" />
+			<Title>Explore repositórios no Github.</Title>
+			<Form>
+				<input type="text" placeholder="Digite o nome do repositório" />
+				<button type="submit">Pesquisar</button>
+			</Form>
+			<Repositories>
+				<a href="/">
+					<img
+						src="https://github.com/ckauan.png"
+						alt="Carlos Kauãn"
+					/>
+					<div>
+						<strong>ckauan/knex-crud</strong>
+						<p>CRUD Postgres feito com knex.js</p>
+					</div>
+					<FiChevronRight size={24} color="#cbcdb6" />
+				</a>
+				<a href="/">
+					<img
+						src="https://github.com/ckauan.png"
+						alt="Carlos Kauãn"
+					/>
+					<div>
+						<strong>ckauan/knex-crud</strong>
+						<p>CRUD Postgres feito com knex.js</p>
+					</div>
+					<FiChevronRight size={24} color="#cbcdb6" />
+				</a>
+				<a href="/">
+					<img
+						src="https://github.com/ckauan.png"
+						alt="Carlos Kauãn"
+					/>
+					<div>
+						<strong>ckauan/knex-crud</strong>
+						<p>CRUD Postgres feito com knex.js</p>
+					</div>
+					<FiChevronRight size={24} color="#cbcdb6" />
+				</a>
+			</Repositories>
+		</>
+	);
 }
