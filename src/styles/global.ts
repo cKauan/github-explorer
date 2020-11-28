3f3be9e3bb6f79450ba67f909183af9ccfd01b5d
carlos <carloskauanmoreiradesousa@gmail.com>
2020-11-28 12:49:42 -0300

    increments global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import githubBackground from '../assets/github-background.svg';
 
 export default createGlobalStyle`
 	* {
@@ -9,11 +10,21 @@ export default createGlobalStyle`
 		text-decoration: none;
 	}
 	body {
-		background-color: #f0f0f5;
+		background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
 		-webkit-font-smoothing: antialiased;
 	}
 
 	body,input,button {
 		font: 16px Roboto, sans-serif;
+	}
+
+	#root {
+		max-width: 960px;
+		margin: 0 auto;
+		padding: 40px 20px;
+	}
+
+	button {
+		cursor: pointer;
 	}
 `;
