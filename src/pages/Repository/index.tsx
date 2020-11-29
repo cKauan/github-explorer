ada5cda6cd015b27b4c2d95045b8c34230b5ae2c
carlos <carloskauanmoreiradesousa@gmail.com>
2020-11-29 17:26:44 -0300

    end repo details page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { Header, RepositoryInfo, Issues } from './styles';
 import githubLogo from '../../assets/github-logo.svg';
+import Api from '../../services/api';
 
 interface Params {
 	repository: string;
+}
+
+interface IRepository {
+	full_name: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+	description: string;
+	stargazers_count: number;
+	forks_count: number;
+	open_issues_count: number;
+}
+interface IIssue {
+	id: number;
+	title: string;
+	user: {
+		login: string;
+	};
+	html_url: string;
 }
 export default function Repository(): JSX.Element {
 	const {
 		params: { repository },
 	} = useRouteMatch<Params>();
+	const [repositoryInfo, setRepositoryInfo] = useState<IRepository | null>(
+		null,
+	);
+	const [issues, setIssues] = useState<IIssue[]>([]);
 
+	useEffect(() => {
+		Api.get(`repos/${repository}`).then(({ data }) => {
+			setRepositoryInfo(data);
+		});
+		Api.get(`repos/${repository}/issues`).then(({ data }) => {
+			setIssues(data);
+		});
+	}, [repository]);
 	return (
 		<>
 			<Header>
@@ -21,40 +54,49 @@ export default function Repository(): JSX.Element {
 					Voltar
 				</Link>
 			</Header>
-			<RepositoryInfo>
-				<header>
-					<img src="https://github.com/ckauan.png" alt="" />
-					<div>
-						<strong>ckauan/ckauan</strong>
-						<p>
-							Back-end student 💻 NodeJS and Typescript |
-							Self-taught
-						</p>
-					</div>
-				</header>
-				<ul>
-					<li>
-						<strong>1808</strong>
-						<span>Stars</span>
-					</li>
-					<li>
-						<strong>48</strong>
-						<span>Forks</span>
-					</li>
-					<li>
-						<strong>67</strong>
-						<span>Issues abertas</span>
-					</li>
-				</ul>
-			</RepositoryInfo>
+			{repositoryInfo && (
+				<RepositoryInfo>
+					<header>
+						<img
+							src={repositoryInfo.owner.avatar_url}
+							alt={repositoryInfo.owner.login}
+						/>
+						<div>
+							<strong>{repositoryInfo.full_name}</strong>
+							<p>{repositoryInfo.description}</p>
+						</div>
+					</header>
+					<ul>
+						<li>
+							<strong>{repositoryInfo.stargazers_count}</strong>
+							<span>Stars</span>
+						</li>
+						<li>
+							<strong>{repositoryInfo.forks_count}</strong>
+							<span>Forks</span>
+						</li>
+						<li>
+							<strong>{repositoryInfo.open_issues_count}</strong>
+							<span>Issues abertas</span>
+						</li>
+					</ul>
+				</RepositoryInfo>
+			)}
 			<Issues>
-				<Link to="/">
-					<div>
-						<strong>Issue</strong>
-						<p>description</p>
-					</div>
-					<FiChevronRight size={24} color="#cbcdb6" />
-				</Link>
+				{issues.map(issue => (
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href={issue.html_url}
+						key={issue.id}
+					>
+						<div>
+							<strong>{issue.title}</strong>
+							<p>{issue.user.login}</p>
+						</div>
+						<FiChevronRight size={24} color="#cbcdb6" />
+					</a>
+				))}
 			</Issues>
 		</>
 	);
