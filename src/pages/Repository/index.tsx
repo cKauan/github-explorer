12ad94b054207b631ee91f9222533f9f9f1e5dbe
carlos <carloskauanmoreiradesousa@gmail.com>
2020-11-29 16:52:56 -0300

    style repo details page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
-import { Header } from './styles';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
 import githubLogo from '../../assets/github-logo.svg';
 
 interface Params {
@@ -13,12 +13,49 @@ export default function Repository(): JSX.Element {
 	} = useRouteMatch<Params>();
 
 	return (
-		<Header>
-			<img src={githubLogo} alt="Github Explorer" />
-			<Link to="/">
-				<FiChevronLeft size={16} />
-				Voltar
-			</Link>
-		</Header>
+		<>
+			<Header>
+				<img src={githubLogo} alt="Github Explorer" />
+				<Link to="/">
+					<FiChevronLeft size={16} />
+					Voltar
+				</Link>
+			</Header>
+			<RepositoryInfo>
+				<header>
+					<img src="https://github.com/ckauan.png" alt="" />
+					<div>
+						<strong>ckauan/ckauan</strong>
+						<p>
+							Back-end student 💻 NodeJS and Typescript |
+							Self-taught
+						</p>
+					</div>
+				</header>
+				<ul>
+					<li>
+						<strong>1808</strong>
+						<span>Stars</span>
+					</li>
+					<li>
+						<strong>48</strong>
+						<span>Forks</span>
+					</li>
+					<li>
+						<strong>67</strong>
+						<span>Issues abertas</span>
+					</li>
+				</ul>
+			</RepositoryInfo>
+			<Issues>
+				<Link to="/">
+					<div>
+						<strong>Issue</strong>
+						<p>description</p>
+					</div>
+					<FiChevronRight size={24} color="#cbcdb6" />
+				</Link>
+			</Issues>
+		</>
 	);
 }
