51f0fb6bb74a758f8bb512cb7307eb44b634f62f
carlos <carloskauanmoreiradesousa@gmail.com>
2020-11-29 16:17:33 -0300

    save data to localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useState, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/github-logo.svg';
 import { Title, Form, Repositories, Error } from './styles';
@@ -15,8 +15,20 @@ interface Repository {
 export default function Dashboard(): JSX.Element {
 	const [repositoryInput, setRepositoryInput] = useState('');
 	const [inputError, setInputError] = useState('');
-	const [repositories, setRepositories] = useState<Repository[]>([]);
+	const [repositories, setRepositories] = useState<Repository[]>(() => {
+		const storagedRepositories = localStorage.getItem(
+			'@GithubExplorer:repositories',
+		);
+		if (storagedRepositories) return JSON.parse(storagedRepositories);
+		return [];
+	});
 
+	useEffect(() => {
+		localStorage.setItem(
+			'@GithubExplorer:repositories',
+			JSON.stringify(repositories),
+		);
+	}, [repositories]);
 	async function handleAddRepository(
 		event: FormEvent<HTMLFormElement>,
 	): Promise<void> {
